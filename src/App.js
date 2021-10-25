a9da31ad69f901a23938abd27a8e3ac725c003dc
Becky Newton <iambexnewton@gmail.com>
2021-10-25 13:06:57 +0200

    added the png to stick to the location

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import './App.css';
 import Map from './components/map';
 
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [longitude, setLongitude] = useState(10.19289);
   const [latitude, setLatitude] = useState(56.11816);
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = async () => {
+    setLoading(true);
+    const res = await axios.get('http://api.open-notify.org/iss-now.json');
+    const { longitude, latitude } = await res.data.iss_position;
+    console.log(res.data);
+
+    setLongitude(parseFloat(longitude));
+    setLatitude(parseFloat(latitude));
+    setLoading(false);
+  };
+
   return (
     <div className="App">
       {!loading ? (
         <Map center={{ lat: latitude, lng: longitude }} />
       ) : (
-        <h1>Loading</h1>
+        <h1>Loading...</h1>
       )}
       <Map />
     </div>
--- src/components/map.js
+++ src/components/map.js
@@ -1,7 +1,4 @@
-import React from 'react';
-
-import { GoogleMapReact } from 'google-map-react';
-
+import GoogleMapReact from 'google-map-react';
 import iss from './iss.png';
 
 const apiKey = process.env.REACT_APP_API_KEY;
@@ -9,14 +6,19 @@ const apiKey = process.env.REACT_APP_API_KEY;
 function Map({ center, zoom }) {
   return (
     <div className="map-container">
-      <googleMapReact
+      <GoogleMapReact
         bootstrapURLKeys={{ key: apiKey }}
         defaultcenter={center}
         defaultZoom={zoom}
-        yesIWantToUseGoogleMapApiInternals
       >
-        <img src={iss} alt="ISS icon" className="iss-icon" />
-      </googleMapReact>
+        <img
+          src={iss}
+          lat={center.lat}
+          lng={center.lng}
+          alt="ISS icon"
+          className="iss-icon"
+        />
+      </GoogleMapReact>
     </div>
   );
 }
@@ -26,7 +28,7 @@ Map.defaultProps = {
     lat: 56.11816,
     lng: 10.19289,
   },
-  zoom: 10,
+  zoom: 6,
 };
 
 export default Map;
